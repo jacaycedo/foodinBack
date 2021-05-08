7ea0a9f18fe4fb464d36a165d71abc10e116a5ad
Santiago Vargas Vega <santiagovargasve@gmail.com>
2021-05-08 07:34:18 -0500

    ADD: Middlewares to control login and access to methods

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,7 +3,7 @@ const config = require('./auth.config')
 const verifyToken = (req, res, next) => {
   let token = req.headers['x-access-token']
   if (!token) {
-    return res.status(403).send({message: 'No tken provided'})
+    return res.status(403).send({message: 'No token provided'})
   }
   jwt.verify(token, config.secret, (err, decoded) => {
     if (err) {
--- routes/foodies.js
+++ routes/foodies.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const router = express.Router('router')
+const verifyToken = require('../middlewares/auth')
+const verifySignUp = require('../middlewares/verifySignUp')
+
 const {
   getFoodies,
   getFoodieByUsername,
@@ -21,7 +24,7 @@ const {
 const multer = require('multer')
 let uploader = multer({dest: 'temp/'})
 
-router.get('/', async function (req, res, next) {
+router.get('/', verifyToken, async function (req, res, next) {
   const foodies = await getFoodies()
   res.send(foodies)
 })
@@ -35,7 +38,7 @@ router.get('/user/:username', async function (req, res, next) {
   }
 })
 
-router.put('/', async function (req, res, next) {
+router.put('/', verifyToken, async function (req, res, next) {
   const foodie = await editarFoodie(req.body)
   res.send(foodie)
 })
@@ -44,10 +47,14 @@ router.get('/recetas/:username', async function (req, res, next) {
   const recipes = await listarRecetas(req.params)
   res.send(recipes)
 })
-router.post('/restaurantesFavoritos', async function (req, res, next) {
-  const restaurants = await insertarRestauranteFavorito(req.body)
-  res.send(restaurants)
-})
+router.post(
+  '/restaurantesFavoritos',
+  verifyToken,
+  async function (req, res, next) {
+    const restaurants = await insertarRestauranteFavorito(req.body)
+    res.send(restaurants)
+  }
+)
 router.get('/restaurantesFavoritos/:username', async function (req, res, next) {
   const restaurants = await listarRestaurantesFavoritos(req.params)
   res.send(restaurants)
@@ -57,17 +64,17 @@ router.get('/recetasFavoritas/:username', async function (req, res, next) {
   const recipes = await listarRecetasFavoritas(req.params)
   res.send(recipes)
 })
-router.post('/recetasFavoritas', async function (req, res, next) {
+router.post('/recetasFavoritas', verifyToken, async function (req, res, next) {
   const recipe = await insertarRecetaFavorita(req.body)
   res.send(recipe)
 })
 
-router.post('/recetas', async function (req, res, next) {
+router.post('/recetas', verifyToken, async function (req, res, next) {
   const recipe = await agregarReceta(req.body)
   res.send(recipe)
 })
 
-router.post('/followRestaurante', async function (req, res, next) {
+router.post('/followRestaurante', verifyToken, async function (req, res, next) {
   const restaurant = await insertarFollowRestaurante(req.body)
   res.send(restaurant)
 })
@@ -77,22 +84,26 @@ router.get('/followRestaurante/:username', async function (req, res, next) {
   res.send(restaurant)
 })
 
-router.post('/followFoodie', async function (req, res, next) {
+router.post('/followFoodie', verifyToken, async function (req, res, next) {
   const foodie = await insertarFollowFoodie(req.body)
   res.send(foodie)
 })
 
-router.get('/followFoodie/:username', async function (req, res, next) {
-  const foodies = await listarFoodiesFollow(req.param)
-  res.send(foodies)
-})
+router.get(
+  '/followFoodie/:username',
+  verifyToken,
+  async function (req, res, next) {
+    const foodies = await listarFoodiesFollow(req.param)
+    res.send(foodies)
+  }
+)
 
-router.post('/categorias', async function (req, res, next) {
+router.post('/categorias', verifyToken, async function (req, res, next) {
   const foodie = await insertarCategoria(req.body)
   res.send(foodie)
 })
 
-router.post('/user/logIn', async function (req, res, next) {
+router.post('/user/logIn', verifyToken, async function (req, res, next) {
   const response = await logIn(req.body)
   console.log(response)
   res.send(response)
@@ -100,7 +111,7 @@ router.post('/user/logIn', async function (req, res, next) {
 
 router.post(
   '/user/signUp',
-  uploader.single('avatar'),
+  [verifySignUp, uploader.single('avatar')],
   async function (req, res, next) {
     try {
       const nuevo = await insertFoodie(req.body, req)
